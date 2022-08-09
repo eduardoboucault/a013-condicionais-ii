be5888c64fb07a0966ea9292f8d994eddaaef589
Eduardo Boucault <boucault.labenu@outlook.com>
2022-08-08 21:36:21 -0300

    Exercício para prática aula 13, aguardando feedback

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,14 @@
-const num = //seu numero aqui
+const num = Number(prompt('Digite um número:'))
 
+if(num % 2 === 0){
+    if(num % 3 === 0){
+        console.log('É divisível por 3')
+    }
+        console.log('É divisível por 2')
+}
+
+if(num % 2 === 0 && num % 3 === 0){
+    console.log('O número digitado é divisível por 2 e por 3.')
+} else {
+    console.log('O número digitado não é divisível por 2 e por 3.')
+}
